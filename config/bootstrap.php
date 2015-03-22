9baaa7911b4f17704da88661e066fef2755506d1
Casper Klenz-Kitenge <hi@cabgfx.com>
2015-03-22 14:06:03 +0100

    Better regex for matching localhost
    + newline fix

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER['REMOTE_ADDR']=='::1' || ereg("^192|10|127", $_SERVER['REMOTE_ADDR']) ) {
+if (preg_match("/^((192|10|127)\.)|::1/", $_SERVER['REMOTE_ADDR']) ) {
     require_once 'environments/development.php';
 } else {
     require_once 'environments/production.php';
