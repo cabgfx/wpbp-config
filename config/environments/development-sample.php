01f496ccc255e38e04d3317a255e053fd94be84a
Casper Klenz-Kitenge <hi@cabgfx.com>
2023-10-10 09:28:39 +0200

    Log to `wp-content/debug.log` instead of on-screen

--- config/environments/development-sample.php
+++ config/environments/development-sample.php
@@ -4,6 +4,9 @@ $dev_server = preg_replace('/:.*/',"", $_SERVER['HTTP_HOST']);
 define('WP_SITEURL', "https://$dev_server");
 define('WP_HOME', "https://$dev_server");
 
+define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
+
 $WP_ENVIRONMENT = array(
 	'db_name' => '',
 	'db_user' => 'root',
