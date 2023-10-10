8a77d0f5af0a38a0df39be14445d3431f14f4cf0
Casper Klenz-Kitenge <hi@cabgfx.com>
2023-10-10 09:27:40 +0200

    Default to use https for development host names

--- config/environments/development-sample.php
+++ config/environments/development-sample.php
@@ -1,8 +1,8 @@
 <?php
 $dev_server = preg_replace('/:.*/',"", $_SERVER['HTTP_HOST']);
 
-define('WP_SITEURL', "http://$dev_server");
-define('WP_HOME', "http://$dev_server");
+define('WP_SITEURL', "https://$dev_server");
+define('WP_HOME', "https://$dev_server");
 
 $WP_ENVIRONMENT = array(
 	'db_name' => '',
